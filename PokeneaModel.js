bbeb981bb54db1833e116f847e7c72a3df736701
Mapa <mpayalal@eafit.edu.co>
2023-10-30 12:01:41 -0500

    Changes in the model

--- PokeneaModel.js
+++ PokeneaModel.js
@@ -2,96 +2,71 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const pokenea = [
-    {
-        id: 1,
-        name: "Rattata",
-        height: 0.3,
-        skill: "Fuga",
-        image: "",
-        phrase: "No mires hacia atrás, Pikachu, el futuro es tan brillante como un día soleado en Medellín.",
-    },
-    
-    {
-        id: 2,
-        name: "Wigglytuff",
-        height: 1.0,
-        skill: "Gran encanto",
-        image: "",
-        phrase: "La diversidad es lo que hace que el mundo Pokémon sea hermoso, al igual que nuestras arepas y empanadas.",
-    },
+class Pokenea{
+    constructor(id, name, height, skill, image, phrase){
+        this.id = id;
+        this.name = name;
+        this.height = height;
+        this.skill = skill;
+        this.image = image;
+        this.phrase = phrase;
+    }
 
-    {
-        id: 3,
-        name: "Gastly",
-        height: 1.3,
-        skill: "Levitación",
-        image: "",
-        phrase: "La vida es como un Pokéball, a veces se abre y otras veces se cierra, pero siempre hay algo valioso adentro.",
-    },
+    get getId(){
+        return this.id;
+    }
 
-    {
-        id: 4,
-        name: "Hitmonchan",
-        height: 1.4,
-        skill: "Vista lince",
-        image: "",
-        phrase: "Recuerda, Bulbasaur, la belleza está en la simplicidad, como una tarde tranquila en el campo paisa.",
-    },
+    set setName(name){
+        this.name = name;
+    }
 
-    {
-        id: 5,
-        name: "Jynx",
-        height: 1.4,
-        skill: "Despiste",
-        image: "",
-        phrase: "Nuestro viaje en la vida es como una Pokédex, siempre buscando nuevos conocimientos y experiencias.",
-    },
+    get getName(){
+        return this.name;
+    }
 
-    {
-        id: 6,
-        name: "Meganium",
-        height: 1.8,
-        skill: "Espesura",
-        image: "",
-        phrase: "La belleza de la naturaleza Pokémon es un reflejo de la belleza en nuestro propio mundo.",
-    },
+    set setHeight(height){
+        this.height = height;
+    }
 
-    {
-        id: 7,
-        name: "Bellossom",
-        height: 0.4,
-        skill: "Clorofila",
-        image: "",
-        phrase: "El poder de la amistad es como la unión de los paisas en una feria de las flores.",
-    },
+    get getHeight(){
+        return this.height;
+    }
 
-    {
-        id: 8,
-        name: "Aipom",
-        height: 0.8,
-        skill: "Fuga",
-        image: "",
-        phrase: "El café caliente en una mañana fría es como un abrazo cálido en el Pueblito Paisa.",
-    },
+    set setSkill(skill){
+        this.skill = skill;
+    }
 
-    {
-        id: 9,
-        name: "Slowking",
-        height: 2.0,
-        skill: "Despiste",
-        image: "",
-        phrase: "El tiempo es como una partida de Pokemón en el parque, siempre en movimiento, siempre presente.",
-    },
+    get getSkill(){
+        return this.skill;
+    }
 
-    {
-        id: 10,
-        name: "Misdreavus",
-        height: 0.7,
-        skill: "Levitación",
-        image: "",
-        phrase: "No importa cuántas veces te caigas, levántate como un Charizard y sigue volando alto.",
-    },
+    set setImage(image){
+        this.image = image;
+    }
 
+    get getImage(){
+        return this.image;
+    }
 
+    set setPhrase(phrase){
+        this.phrase = phrase;
+    }
+
+    get getPhrase(){
+        return this.phrase;
+    }
+
+}
+
+pokenea = [
+    new Pokenea(1, "Rattata", 0.3, "Fuga", "https://storage.googleapis.com/pokeneas_fotos/Rattata.png", "No mires hacia atrás, Pikachu, el futuro es tan brillante como un día soleado en Medellín."),
+    new Pokenea(2, "Wigglytuff", 1.0, "Gran encanto", "https://storage.googleapis.com/pokeneas_fotos/Wigglytuff.png","La diversidad es lo que hace que el mundo Pokémon sea hermoso, al igual que nuestras arepas y empanadas."),
+    new Pokenea(3, "Gastly", 1.3, "Levitación", "https://storage.googleapis.com/pokeneas_fotos/Gastly.png", "La vida es como un Pokéball, a veces se abre y otras veces se cierra, pero siempre hay algo valioso adentro."),
+    new Pokenea(4, "Hitmonchan", 1.4, "Vista lince", "https://storage.googleapis.com/pokeneas_fotos/Hitmonchan.png", "Recuerda, Bulbasaur, la belleza está en la simplicidad, como una tarde tranquila en el campo paisa."),
+    new Pokenea(5, "Jynx", 1.4, "Despiste", "https://storage.googleapis.com/pokeneas_fotos/Jynx.png", "Nuestro viaje en la vida es como una Pokédex, siempre buscando nuevos conocimientos y experiencias."),
+    new Pokenea(6, "Meganium", 1.8, "Espesura", "https://storage.googleapis.com/pokeneas_fotos/Meganium.png", "La belleza de la naturaleza Pokémon es un reflejo de la belleza en nuestro propio mundo."),
+    new Pokenea(7, "Bellossom", 0.4, "Clorofila", "https://storage.googleapis.com/pokeneas_fotos/Bellossom.png", "El poder de la amistad es como la unión de los paisas en una feria de las flores."),
+    new Pokenea(8, "Aipom", 0.8, "Fuga", "https://storage.googleapis.com/pokeneas_fotos/Aipom.png", "El café caliente en una mañana fría es como un abrazo cálido en el Pueblito Paisa."),
+    new Pokenea(9, "Slowking", 2.0, "Despiste", "https://storage.googleapis.com/pokeneas_fotos/Slowking.png", "El tiempo es como una partida de Pokemón en el parque, siempre en movimiento, siempre presente."),
+    new Pokenea(10, "Misdreavus", 0.7, "Levitación", "https://storage.googleapis.com/pokeneas_fotos/Misdreavus.png", "No importa cuántas veces te caigas, levántate como un Charizard y sigue volando alto.")
 ]
